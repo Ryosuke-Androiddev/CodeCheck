980a0ca514cb52420c9ec99018c0339b1fde145e
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-01-28 00:53:00 +0900

    #1 [modify] change Adapter name and add comment

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragment.kt
@@ -30,7 +30,7 @@ class SearchFragment: Fragment(R.layout.fragment_search){
         val dividerItemDecoration=
             DividerItemDecoration(context!!, layoutManager.orientation)
 
-        val adapter= CustomAdapter(object : CustomAdapter.OnItemClickListener{
+        val adapter= ItemListAdapter(object : ItemListAdapter.OnItemClickListener{
 
             // TODO this viewModel fun is comment out now
 //            override fun itemClick(DetailItem: DetailItem){
@@ -93,9 +93,9 @@ val Diff_UTIL_ITEM_CALLBACK = object: DiffUtil.ItemCallback<DetailItem>(){
 }
 
 //表示するデータの型DetailItem，次に, ViewHolderの型を指定する
-class CustomAdapter(
+class ItemListAdapter(
     private val itemClickListener: OnItemClickListener,
-) : ListAdapter<DetailItem, CustomAdapter.ViewHolder>(Diff_UTIL_ITEM_CALLBACK){
+) : ListAdapter<DetailItem, ItemListAdapter.ViewHolder>(Diff_UTIL_ITEM_CALLBACK){
 
     //bind fun で、Viewと受け取ったObjectを結び付ける
     class ViewHolder(view: View): RecyclerView.ViewHolder(view)
@@ -106,20 +106,24 @@ class CustomAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
+        // 生成されたBinding Classを使えば、fromが必要なくなる?　そもそも、fromの意味とは?
     	val view= LayoutInflater.from(parent.context)
             .inflate(R.layout.layout_item, parent, false)
 
     	return ViewHolder(view)
     }
 
+    // 生成されたBinding Classを引数で渡す。
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
+        //holder.bind(getItem(position))
     	val item= getItem(position)
 
-        //ここで強制キャストが行われてるのか
+        //ここで強制キャストが行われてるのか この処理は、フルネームを表示するためのものである。
         (holder.itemView.findViewById<View>(R.id.repositoryNameView) as TextView).text=
             item.name
 
+        // click されたときに、何を渡すのかを決める必要がある
     	holder.itemView.setOnClickListener{
      		itemClickListener.itemClick(item)
     	}
