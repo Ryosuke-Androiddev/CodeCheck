ab1af0607eef06a8e00561b20e68ed6891e7f91c
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-01-27 16:25:30 +0900

    #1 [modify] change variable name check If binding class gnenerate

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/DetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/DetailFragment.kt
@@ -6,17 +6,19 @@ package jp.co.yumemi.android.code_check
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.databinding.DataBindingUtil.bind
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import coil.load
 import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
+import jp.co.yumemi.android.code_check.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
 
-    //private val args: TwoFragmentArgs by navArgs()
+    private val args: DetailFragmentArgs by navArgs()
 
-//    private var binding: FragmentTwoBinding? = null
-//    private val _binding get() = binding!!
-
-    private var binding: DetailFragment? = null
+    // view inflate
+    private var binding: FragmentDetailBinding? = null
     private val _binding get() = binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,16 +26,18 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
         Log.d("検索した日時", lastSearchDate.toString())
 
-        //binding = FragmentTwoBinding.bind(view)
+        binding = FragmentDetailBinding.bind(view)
 
-//        var item = args.item
-//
-//        _binding.ownerIconView.load(item.ownerIconUrl);
-//        _binding.nameView.text = item.name;
-//        _binding.languageView.text = item.language;
-//        _binding.starsView.text = "${item.stargazersCount} stars";
-//        _binding.watchersView.text = "${item.watchersCount} watchers";
-//        _binding.forksView.text = "${item.forksCount} forks";
-//        _binding.openIssuesView.text = "${item.openIssuesCount} open issues";
+        // this is navigated item
+        var item = args.item
+
+        // set info into view using viewBinding
+        _binding.ownerIconView.load(item.ownerIconUrl);
+        _binding.nameView.text = item.name;
+        _binding.languageView.text = item.language;
+        _binding.starsView.text = "${item.stargazersCount} stars";
+        _binding.watchersView.text = "${item.watchersCount} watchers";
+        _binding.forksView.text = "${item.forksCount} forks";
+        _binding.openIssuesView.text = "${item.openIssuesCount} open issues";
     }
 }
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragment.kt
@@ -7,9 +7,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
+import jp.co.yumemi.android.code_check.databinding.FragmentSearchBinding
 
 class SearchFragment: Fragment(R.layout.fragment_search){
 
@@ -17,45 +20,50 @@ class SearchFragment: Fragment(R.layout.fragment_search){
     {
         super.onViewCreated(view, savedInstanceState)
 
-        //val _binding= FragmentOneBinding.bind(view)
+        val binding= FragmentSearchBinding.bind(view)
 
-//        val _viewModel= SearchViewModel(context!!)
-//
-//        val _layoutManager= LinearLayoutManager(context!!)
-//        val _dividerItemDecoration=
-//            DividerItemDecoration(context!!, _layoutManager.orientation)
-//        val _adapter= CustomAdapter(object : CustomAdapter.OnItemClickListener{
-//            override fun itemClick(item: item){
-//                gotoRepositoryFragment(item)
-//            }
-//        })
+        val viewModel= SearchViewModel(context!!)
 
-//        _binding.searchInputText
-//            .setOnEditorActionListener{ editText, action, _ ->
-//                if (action== EditorInfo.IME_ACTION_SEARCH){
-//                    editText.text.toString().let {
-//                        _viewModel.searchResults(it).apply{
+        val layoutManager= LinearLayoutManager(context!!)
+
+        val dividerItemDecoration=
+            DividerItemDecoration(context!!, layoutManager.orientation)
+
+        val adapter= CustomAdapter(object : CustomAdapter.OnItemClickListener{
+            override fun itemClick(DetailItem: DetailItem){
+                gotoRepositoryFragment(DetailItem)
+            }
+        })
+
+        binding.searchInputText
+            .setOnEditorActionListener{ editText, action, _ ->
+                if (action== EditorInfo.IME_ACTION_SEARCH){
+                    editText.text.toString().let {
+                        // TODO this viewModel fun is comment out now
+//                        viewModel.searchResults(it).apply{
 //                            _adapter.submitList(this)
 //                        }
-//                    }
-//                    return@setOnEditorActionListener true
-//                }
-//                return@setOnEditorActionListener false
-//            }
-//
-//        _binding.recyclerView.also{
-//            it.layoutManager= _layoutManager
-//            it.addItemDecoration(_dividerItemDecoration)
-//            it.adapter= _adapter
-//        }
+                    }
+                    return@setOnEditorActionListener true
+                }
+                return@setOnEditorActionListener false
+            }
+
+        binding.recyclerView.also{
+            it.layoutManager= layoutManager
+            it.addItemDecoration(dividerItemDecoration)
+            it.adapter= adapter
+        }
     }
 
-//    fun gotoRepositoryFragment(item: item)
-//    {
-//        val _action= OneFragmentDirections
-//            .actionRepositoriesFragmentToRepositoryFragment(item= item)
-//        findNavController().navigate(_action)
-//    }
+    fun gotoRepositoryFragment(DetailItem: DetailItem)
+    {
+        val action= SearchFragmentDirections
+            .actionRepositoriesFragmentToRepositoryFragment(item= DetailItem)
+
+        // Navigate to DetailFragment with SafeArgs
+        findNavController().navigate(action)
+    }
 }
 
 val diff_util= object: DiffUtil.ItemCallback<DetailItem>(){
@@ -83,19 +91,19 @@ class CustomAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
     {
-    	val _view= LayoutInflater.from(parent.context)
+    	val view= LayoutInflater.from(parent.context)
             .inflate(R.layout.layout_item, parent, false)
-    	return ViewHolder(_view)
+    	return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int)
     {
-    	val _item= getItem(position)
+    	val item= getItem(position)
         (holder.itemView.findViewById<View>(R.id.repositoryNameView) as TextView).text=
-            _item.name
+            item.name
 
     	holder.itemView.setOnClickListener{
-     		itemClickListener.itemClick(_item)
+     		itemClickListener.itemClick(item)
     	}
     }
 }
