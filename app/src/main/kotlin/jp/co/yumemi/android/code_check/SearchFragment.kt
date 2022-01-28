6ba8f85a6c9ba885a66a4b2aa7e1399dc502a696
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-01-28 13:07:06 +0900

    #1 [modify] solve nest logic at ViewHolder class and change arguments name at navigation fun and nav graph

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragment.kt
@@ -10,11 +10,12 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
 import jp.co.yumemi.android.code_check.databinding.FragmentSearchBinding
 import jp.co.yumemi.android.code_check.databinding.LayoutItemBinding
 
-class SearchFragment: Fragment(R.layout.fragment_search){
+class SearchFragment: Fragment(R.layout.fragment_search) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
@@ -31,7 +32,7 @@ class SearchFragment: Fragment(R.layout.fragment_search){
         val dividerItemDecoration=
             DividerItemDecoration(requireContext(), layoutManager.orientation)
 
-        val adapter= ItemListAdapter(object : ItemListAdapter.OnItemClickListener{
+        val adapter= ItemListAdapter(object : ItemListAdapter.OnItemClickListener {
 
             // TODO this viewModel fun is comment out now
 //            override fun itemClick(DetailItem: DetailItem){
@@ -40,8 +41,8 @@ class SearchFragment: Fragment(R.layout.fragment_search){
         })
 
         binding.searchInputText
-            .setOnEditorActionListener{ editText, action, _ ->
-                if (action== EditorInfo.IME_ACTION_SEARCH){
+            .setOnEditorActionListener { editText, action, _ ->
+                if (action == EditorInfo.IME_ACTION_SEARCH) {
                     editText.text.toString().let {
                         // TODO this viewModel fun is comment out now
 //                        viewModel.searchRepository(it).apply{
@@ -61,18 +62,18 @@ class SearchFragment: Fragment(R.layout.fragment_search){
     }
 
     // TODO this viewModel fun is comment out now
-//    fun navigateToDetailWithArgs(DetailItem: DetailItem) {
-//
-//        val action= SearchFragmentDirections
-//            .actionRepositoriesFragmentToRepositoryFragment(item= DetailItem)
-//
-//        // Navigate to DetailFragment with SafeArgs
-//        findNavController().navigate(action)
-//    }
+    fun navigateToDetailWithArgs(detailItem: DetailItem) {
+
+        val action= SearchFragmentDirections
+            .actionSearchFragmentToDetailFragment(detailItem)
+
+        // Navigate to DetailFragment with SafeArgs
+        findNavController().navigate(action)
+    }
 }
 
 // 作成したDiffUtilは，ViewHolderの中で呼び出しを行う
-val Diff_UTIL_ITEM_CALLBACK = object: DiffUtil.ItemCallback<DetailItem>(){
+val Diff_UTIL_ITEM_CALLBACK = object: DiffUtil.ItemCallback<DetailItem>() {
 
     // オブジェクト特有の値を用いて，比較を行う．今回は，それぞれのオブジェクトの名前の比較を行う
     override fun areItemsTheSame(
@@ -96,7 +97,7 @@ val Diff_UTIL_ITEM_CALLBACK = object: DiffUtil.ItemCallback<DetailItem>(){
 //表示するデータの型DetailItem，次に, ViewHolderの型を指定する
 class ItemListAdapter(
     private val itemClickListener: OnItemClickListener,
-) : ListAdapter<DetailItem, DetailItemViewHolder>(Diff_UTIL_ITEM_CALLBACK){
+) : ListAdapter<DetailItem, DetailItemViewHolder>(Diff_UTIL_ITEM_CALLBACK) {
 
     interface OnItemClickListener {
     	fun itemClick(DetailItem: DetailItem)
@@ -114,22 +115,24 @@ class ItemListAdapter(
     // 生成されたBinding Classを引数で渡す。
     override fun onBindViewHolder(holderDetailItem: DetailItemViewHolder, position: Int) {
 
-        holderDetailItem.bind(getItem(position))
-
-        // click されたときに、何を渡すのかを決める必要がある
-    	holderDetailItem.itemView.setOnClickListener{
-     		itemClickListener.itemClick(getItem(position))
-    	}
+        holderDetailItem.bind(getItem(position), itemClickListener)
     }
 }
 
 //bind fun で、Viewと受け取ったObjectを結び付ける
 class DetailItemViewHolder(
-    private val binding: LayoutItemBinding
-): RecyclerView.ViewHolder(binding.root){
+    private val binding: LayoutItemBinding,
+): RecyclerView.ViewHolder(binding.root) {
 
     // Objectを渡して、このメソッドでバインドする
-    fun bind(detailItem: DetailItem) {
+    fun bind(
+        detailItem: DetailItem,
+        itemClickListener: ItemListAdapter.OnItemClickListener
+    ) {
         binding.repositoryNameView.text = detailItem.name
+
+        binding.repositoryNameView.setOnClickListener {
+            itemClickListener.itemClick(detailItem)
+        }
     }
 }
