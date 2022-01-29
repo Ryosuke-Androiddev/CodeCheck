c6f7cf00f35e7e6b67bac2de93920489a7c33978
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-01-29 15:07:09 +0900

    #4 [separate] separate class and package

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/api/GithubApi.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/api/GithubApi.kt
@@ -1,9 +1,7 @@
 package jp.co.yumemi.android.code_check.api
 
-import jp.co.yumemi.android.code_check.model.ApiResult
-import retrofit2.Response
+import jp.co.yumemi.android.code_check.model.dto.ApiResult
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface GithubApi {
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/model/dto/ApiResult.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/model/dto/ApiResult.kt
@@ -1,9 +1,9 @@
-package jp.co.yumemi.android.code_check.model
+package jp.co.yumemi.android.code_check.model.dto
 
 
 import android.util.Log
 import com.google.gson.annotations.SerializedName
-import jp.co.yumemi.android.code_check.DetailItem
+import jp.co.yumemi.android.code_check.model.DetailItem
 
 data class ApiResult(
     @SerializedName("items")
